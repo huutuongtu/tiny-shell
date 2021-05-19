8f9dc7eb5e572f711f887c6ccbced06941298919
huutuongtu <83907151+huutuongtu@users.noreply.github.com>
2021-05-19 14:08:02 +0700

    Update tiny-shell.cpp

--- tiny-shell.cpp
+++ tiny-shell.cpp
@@ -1,142 +1,193 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <windows.h>
-#include <signal.h>
-#include <conio.h>
 #include <iostream>
-#include <cstdio>
-#include <dirent.h>
-#include <iostream>
-#include <string>
+#include <time.h>
+#include <string.h>
 
-char command_buf[500];
-
-char key;
+std::string command_buf;
 STARTUPINFO si[100];
 PROCESS_INFORMATION  pi[100];
 int n, background_mode = 1;
-void menu(){
-	printf("							MY-SHELL\n");
-	printf("________________________________________________________________________________________________________________________\n \n");
-	printf("help: Show menu\n");
+void menu() {
+    std::cout << "                                                    MY-SHELL" << std::endl;
+    std::cout << "___________________________________________________________________________________________________________________\n" << std::endl;
+    std::cout << "help: Show menu" << std::endl;
 }
-void kill_all(){
-    for (int i = 1;i <= n; i++){
-        TerminateProcess(pi[i].hProcess, 1);CloseHandle(pi[i].hProcess);CloseHandle(pi[i].hThread);
+    void kill_all() {
+        for (int i = 1; i <= n; i++) {
+            TerminateProcess(pi[i].hProcess, 1);
+            CloseHandle(pi[i].hProcess);
+            CloseHandle(pi[i].hThread);
+        }
+        std::cout << "All processes killed..." << std::endl;
+        n = 0;
     }
-    printf("All process killed ... \n");
-    n = 0;
-}
-void kill(int id){
-    for (int i = 1;i <= n; i++){
-        if (id == pi[i].dwProcessId){
-            TerminateProcess(pi[i].hProcess, 1);CloseHandle(pi[i].hProcess);CloseHandle(pi[i].hThread);
-            for (int j = i; j < n; j++) pi[j] = pi[j + 1], si[j] = si[j + 1];
-            n--;
-            break;
+        void kill_id(int id) {
+            for (int i = 1; i <= n; i++) {
+                if (id == pi[i].dwProcessId) {
+                    TerminateProcess(pi[i].hProcess, 1);
+                    CloseHandle(pi[i].hProcess);
+                    CloseHandle(pi[i].hThread);
+
+                    for (int j = i; j < n; j++) {
+                        pi[j] = pi[j + 1];
+                        si[j] = si[j + 1];
+                    }
+                    n--;
+                    break;
+                }
+            }
         }
-    }
-}
 
-void child(){
-    ++n;
-    ZeroMemory(&si[n], sizeof(si[n]));
-    si[n].cb = sizeof(si[n]);
-    CreateProcess("child.exe",NULL,NULL,NULL,FALSE,CREATE_NEW_CONSOLE,NULL,NULL,&si[n],&pi[n]);
-    if (background_mode == 1) return;
-    WaitForSingleObject(pi[n].hProcess,10000);
-}
-void list(){
-    for (int i = 1;i <= n; i++)
-        printf("%d %p %s\n", pi[i].dwProcessId, pi[i].hProcess, si[i].wShowWindow);
-}
-void calc(){
-    ++n;
-    ZeroMemory(&si[n], sizeof(si[n]));
-    si[n].cb = sizeof(si[n]);
-    CreateProcess("C:\\Windows\\System32\\calc.exe",NULL,NULL,NULL,FALSE,CREATE_NEW_CONSOLE,NULL,NULL,&si[n],&pi[n]);
-    if (background_mode == 1) return;
-    WaitForSingleObject(pi[n].hProcess,10000);
-}
-void exit_process(){
-    printf("Sending signal to kill all child processes ...\n");
-    kill_all();
-
-}
-void sigint(int a){   
-    printf("Killing foreground process ... \n");
-    for (int i = 1;i <= n; i++){
-        TerminateProcess(pi[i].hProcess, 1);CloseHandle(pi[i].hProcess);CloseHandle(pi[i].hThread);
-    }
-    n = 0;
-}
-
-void help() {
-    // Display all commands
-    printf("For more information on a specific command, type HELP command: name\n");
-    printf("kill -1    Kill all running processes\n");
-    printf("kill 'ID'   Kill a running process with id \n");
-    printf("date    Displays or sets the date\n");
-    printf("calc    Open system calculator\n");
-    printf("list    Displays list of commands\n");
-    printf("Dir Get the list of files in directory\n");
-	printf("help    ");
-    printf("child  Open child process\n");
-    
-}
-
-
-void open(){
-}
-
-void dir(){
-}
-
-
-
-
-int main(){
-    menu();
-	while(1) 
-	{
-		printf("Keyword:  ");
-        //if (!fgets(command_buf, 128, stdin)) break;
-        scanf("%s", &command_buf);
-		if(!strcmp(command_buf, "ipconfig")) { system("ipconfig"); }
-		else if(!strcmp(command_buf, "date")) { system("date"); }
-		else if(!strcmp(command_buf, "exit") || !strcmp(command_buf, "quit") || !strcmp(command_buf, "e") || !strcmp(command_buf, "q")) {
-            exit_process();
-            return 0;
-        }
-		else if(!strcmp(command_buf, "child")) {
-            child();
-        }
-        else if(!strcmp(command_buf, "calc")) {
-            calc();
-        }
-        else if (!strcmp(command_buf, "kill")){
+        void kill() {
             int id;
-            scanf("%d", &id);
-            if (id == -1) kill_all();
-            else kill(id);
+            std::cout << "Enter process id to kill process, or -1 to kill all processes\n";
+            std::cin >> id;
+            if (id == -1) {
+                kill_all();
+            }
+            else {
+                kill_id(id);
+            }
+
         }
-        else if (!strcmp(command_buf, "background")){
-            scanf("%s", command_buf);
-            if (!strcmp(command_buf, "on")) background_mode = 1;
-            if (!strcmp(command_buf, "off")) background_mode = 0;
+
+        void child() {
+            n++;
+            ZeroMemory(&si[n], sizeof(si[n]));
+            si[n].cb = sizeof(si[n]);
+            const char* p = "child.exe";
+            
+            const WCHAR* pwcsName; //LPCWSTR
+
+            // required size
+            int size = MultiByteToWideChar(CP_ACP, 0, p, -1, NULL, 0);
+            // allocate it
+            pwcsName = new WCHAR[999];
+            MultiByteToWideChar(CP_ACP, 0, p, -1, (LPWSTR)pwcsName, size);
+            CreateProcess(pwcsName, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si[n], &pi[n]);
+            if (background_mode == 1) {
+                return;
+            }
+            WaitForSingleObject(pi[n].hProcess, 10000);
         }
-        else if (!strcmp(command_buf, "list")){
-            list();
+
+        void calc() {
+            n++;
+            ZeroMemory(&si[n], sizeof(si[n]));
+            si[n].cb = sizeof(si[n]);
+            const char* k = "calc.exe";
+
+            const WCHAR* pwcsName; //LPCWSTR
+
+            // required size
+            int size = MultiByteToWideChar(CP_ACP, 0, k, -1, NULL, 0);
+            // allocate it
+            pwcsName = new WCHAR[999];
+            MultiByteToWideChar(CP_ACP, 0, k, -1, (LPWSTR)pwcsName, size);
+            CreateProcess(pwcsName, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si[n], &pi[n]);
+            if (background_mode == 1) {
+                return;
+            }
+            WaitForSingleObject(pi[n].hProcess, 10000);
         }
-        else if (!strcmp(command_buf, "help")){
-        	help();
-		}
-		else if (!strcmp(command_buf, "open")){
-		
-			
-		}
-		else {printf("404 Error not Found !!!");
-		}
-	}
-}
+        void list() {
+            for (int i = 1; i <= n; i++) {
+                std::cout << pi[i].dwProcessId << " "
+                    << pi[i].hProcess << " "
+                    << si[i].wShowWindow << std::endl;
+            }
+        }
+
+        void open() {
+            std::string file_name;
+            std::cout << "Please enter the file path name: ";
+            std::cin >> file_name;
+            std::string openString = "start " + file_name;
+            system(openString.c_str());
+        }
+        void dir() {
+            std::string path;
+            std::cout << "Please enter the directory: ";
+            std::cin >> path;
+            std::string openString = "dir " + path;
+            system(openString.c_str());
+        }
+
+        void exit() {
+            std::cout << "Sending signal to kill all child processes..." << std::endl;
+            kill_all();
+        }
+
+       
+
+        void help() {
+            std::cout << "For more information on a specific command, type HELP command: name" << std::endl;
+            std::cout << "kill -1     Kill all running processes" << std::endl;
+            std::cout << "kill 'ID'   Kill a running process with id " << std::endl;
+            std::cout << "child       Open child process" << std::endl;
+            std::cout << "calc        Open system calculator" << std::endl;
+            std::cout << "list        Displays list of commands" << std::endl;
+            std::cout << "open        Open a file" << std::endl;
+            std::cout << "dir         Get the list of files and subdirectories in directory" << std::endl;
+            std::cout << "exit        Exit shell" << std::endl;
+            std::cout << "date        Displays or sets the date" << std::endl;
+            std::cout << "help        Display all commands" << std::endl;
+            std::cout << "clear       Clear the terminal" << std::endl;
+        
+        }
+    
+
+        int main() {
+            menu();
+            while (1) {
+                std::cout << "Keyword:  ";
+                std::cin >> command_buf;
+                if (!command_buf.compare("kill")) {
+                    kill();
+                }
+                else if (!command_buf.compare("child")) {
+                    child();
+                }
+                else if (!command_buf.compare("calc")) {
+                    calc();
+                }
+                else if (!command_buf.compare("list")) {
+                    list();
+                }
+                else if (!command_buf.compare("open")) {
+                    open();
+                }
+                else if (!command_buf.compare("dir")) {
+                    dir();
+                }
+                else if (!command_buf.compare("exit") || !command_buf.compare("e")) {
+                    exit();
+                    return 0;
+                }
+                else if (!command_buf.compare("date")) {
+                    system("date");
+                }
+                else if (!command_buf.compare("help")) {
+                    help();
+                }
+                else if (!command_buf.compare("ipconfig")) {
+                    system("ipconfig");
+                }
+                else if (!command_buf.compare("background")) {
+                    std::cin >> command_buf;
+                    if (!command_buf.compare("on")) {
+                        background_mode = 1;
+                    }
+                    if (!command_buf.compare("off")) {
+                        background_mode = 0;
+                    }
+                }
+                else if (!command_buf.compare("clear")) {
+                    system("cls");
+                }
+
+                else {
+                    std::cout << "404 Error not Found !!!" << std::endl;
+                }
+            }
+        }
